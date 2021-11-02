0963816bbff477e18e60424e926da624bf6c98cc
Kapelushny <zig.tos@gmail.com>
2021-11-03 00:33:38 +0200

    small fix

--- src/test/java/com/study/iostreams/ByteArrayOutputStreamTest.java
+++ src/test/java/com/study/iostreams/ByteArrayOutputStreamTest.java
@@ -1,6 +1,5 @@
 package com.study.iostreams;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,10 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ByteArrayOutputStreamTest {
     private final String STREAM_CLOSED = "Stream is dead";
-    private final byte[] emptyArray = new byte[]{};
-    private final byte[] oneArray = new byte[]{1};
-    private final byte[] twoArray = new byte[]{1, 2};
-    private final byte[] threeArray = new byte[]{1, 2, 3};
     private byte[] fiveArray = new byte[]{1, 2, 3, 4, 5};
 
 
@@ -28,19 +23,16 @@ class ByteArrayOutputStreamTest {
     }
 
     @Test
-    @Disabled
     @DisplayName("When call write correct data returned")
     void whenWrite() throws IOException {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         os.write(1);
         assertEquals(1, os.getArray()[0]);
-        assertEquals(0, os.getArray()[1]);
         os.write(2);
         assertEquals(2, os.getArray()[1]);
     }
 
     @Test
-    @Disabled
     @DisplayName("When write byte[] then all bytes are in array")
     void whenWrite1() throws IOException {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -49,7 +41,6 @@ class ByteArrayOutputStreamTest {
     }
 
     @Test
-    @Disabled
     @DisplayName("When write byte[] with offset and length then correct bytes are in array")
     void whenReadBytes() throws IOException {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
