d6c049d2a7f6d629220108da73a168424a12b9b8
Kapelushny <zig.tos@gmail.com>
2021-11-04 11:00:23 +0200

    add 404 case

--- src/main/java/com/study/webserver/Server.java
+++ src/main/java/com/study/webserver/Server.java
@@ -10,12 +10,12 @@ import java.nio.charset.StandardCharsets;
 @Data
 public class Server {
     private static final String EXCEPTION_IN_STREAM = "Something went wrong in 'readContent(String path)')!";
+    static byte[] array = new byte[999];
     int port;
     String webappPath;
-    static byte[] array = new byte[999];
 
     static int readContent(String path) {
-        try (var bufferedReader =new FileInputStream(path)) {
+        try (var bufferedReader = new FileInputStream(path)) {
             return bufferedReader.read(array);
         } catch (IOException e) {
             throw new RuntimeException(EXCEPTION_IN_STREAM, e);
@@ -26,29 +26,30 @@ public class Server {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println(" === Server start to work === ");
 
-                for (;;) {
-                    try (Socket socket = serverSocket.accept();
-                         BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                         OutputStream writer = socket.getOutputStream()) {
-                        String uri;
+            for (; ; ) {
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     OutputStream writer = socket.getOutputStream()) {
 
                     StringBuilder sb = new StringBuilder();
                     String line;
                     while (!(line = reader.readLine()).isEmpty()) {
                         sb.append(line).append(System.getProperty("line.separator"));
                     }
-                    System.out.println(sb);
-                    uri = sb.toString().split(" ")[1];
+                    String uri = sb.toString().split(" ")[1];
                     String path = webappPath + uri;
                     System.out.println("Ready to write response " + path);
 
-                    int count = readContent(path);
-                    System.out.println(array);
+                    if (!new File(path).exists()) {
+                        writeHeader(writer);
+                        writer.write("Error 404 file not found".getBytes(StandardCharsets.UTF_8));
+                        writer.flush();
+                    } else {
+                        int count = readContent(path);
 
-                    writer.write("HTTP/1.1 200 OK".getBytes(StandardCharsets.UTF_8));
-                    writer.write(System.getProperty("line.separator").getBytes(StandardCharsets.UTF_8));
-                    writer.write(System.getProperty("line.separator").getBytes(StandardCharsets.UTF_8));
-                    writer.write(array, 0, count);
+                        writeHeader(writer);
+                        writer.write(array, 0, count);
+                    }
                     writer.flush();
                     System.out.println("written");
                 }
@@ -57,5 +58,11 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void writeHeader(OutputStream writer) throws IOException {
+        writer.write("HTTP/1.1 200 OK".getBytes(StandardCharsets.UTF_8));
+        writer.write(System.getProperty("line.separator").getBytes(StandardCharsets.UTF_8));
+        writer.write(System.getProperty("line.separator").getBytes(StandardCharsets.UTF_8));
     }
 }
